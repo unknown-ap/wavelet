4535ca818d85262a1170949a376f9beee2a5c9cc
unknown-ap <annie.ping09@gmail.com>
2023-01-18 16:30:53 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> words = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+            //finish this section
+        }
+    }
+}
+
+public class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
